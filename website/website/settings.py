a996aa808381c9cca25d68253d78cadedd93a90f
okoriek <66570436+okoriek@users.noreply.github.com>
2023-09-04 05:16:33 +0100

    Update settings

--- website/website/settings.py
+++ website/website/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3mjg9&2kidb4ix+b8$lq=@&t#9+%c_5407gobdv#x)u8xseojf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
